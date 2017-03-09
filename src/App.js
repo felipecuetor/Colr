ab58914bc96504364c103f253ed2487c138c0408
Felipe Cueto Ramirez <f.cueto10@sis.virtual.uniandes.edu.co>
2017-03-09 14:04:54 -0500

    man

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Fotos from './Fotos.js';
+import axios from 'axios';
 import './App.css';
+const ROOT_URL = "http://localhost:9000";
+
 
 class App extends Component {
 	
 	constructor(props) {
         super(props);
-
         console.log("Start")
         this.state = {
             imagenes: [],
-
         };
+		
+		
 
+		this.obtenerImagesCriterio("familia");
     }
 	
 	
     obtenerImagesCriterio(cri) {
     {
+		
+		console.log("Buscando con criterio: "+ cri);
+		
+		{
+            axios.get(ROOT_URL + "/flickr/" + cri)
+                .then(response => {
+                    this.setState({
+                        imagenes: response.data
+                    });
+                })
+        }
     }
     }
 	
   render() {
     return (
       <div className="App">
+	  <add name="Access-Control-Allow-Origin" value="*"/>
         <div className="App-header">
-          <h2>Callr</h2>
+          <h1>Callr</h1>
         </div>
 		<body>
 		<input type="text" onChange={(event) => this.obtenerImagesCriterio(event.target.value)}/>
         <p className="App-intro">
-			<Fotos/>
+			{this.state.imagenes.map(imagesData => < Fotos key = { imagesData.id } {...imagesData }/>)}
         </p>
-		<script src="../js/script.js"></script>
 		</body>
       </div>
     );
--- src/Fotos.js
+++ src/Fotos.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 //import './Fotos.css';
 
 class Fotos extends Component {
+	
+	darURL(url)
+	{
+		var resp = "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_s.jpg";
+		resp.replace("{farm-id}", url.farm);
+		resp.replace("{server-id}", url.server);
+		resp.replace("{id}", url.id);
+		resp.replace("{secret}", url.secret);
+		return resp;
+	}
+	
+	
   render() {
     return (
       <div className="Foto">
-		<img src={`${this.props.url}`}/>
+		<img src={this.darURL()}/>
       </div>
     );
   }
